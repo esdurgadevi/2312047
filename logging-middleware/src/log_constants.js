a61d7427b5b42dc66040ce972e6f96329c6c4fee
esdurgadevi <esdurgadevi1@gmail.com>
2026-07-02 10:36:07 +0530

    Logging-middleware: add logging constants

---
+++ logging-middleware/src/log_constants.js
@@ -0,0 +1,9 @@
+export const CONSTANTS = {
+    stack : ['backend','frontend'],
+    level : ['debug','info','warn','error','fatal'],
+    package : {
+        backend : ['cache','contoller','cron_job','db','domain','handler','repository','route','service'],
+        frontend : ['api','component','hook','page','state','style'],
+        shared : ['auth','config','middleware','utils']
+    }
+}
