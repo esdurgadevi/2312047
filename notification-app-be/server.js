7cec79ceebbd8ccd83b8f0e88a1efb6a989b787c
esdurgadevi <esdurgadevi1@gmail.com>
2026-07-02 12:10:21 +0530

    Backend Initial server setup

---
+++ notification-app-be/server.js
@@ -0,0 +1,29 @@
+import express from "express"
+import dotenv from "dotenv";
+import { logger } from "../logging-middleware/src/index.js";
+dotenv.config()
+
+const app = express();
+app.use(express.json());
+const port = process.env.PORT;
+app.post("/log", async (req, res) => {
+
+    try {
+
+        const response = await logger(req.body);
+
+        return res.status(200).json(response);
+
+    } catch (error) {
+
+        return res.status(500).json({
+            error: error.message
+        });
+
+    }
+
+});
+
+app.listen(port,()=>{
+    console.log(`Server run on ${port}`);
+})
