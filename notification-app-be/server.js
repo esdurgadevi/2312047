e6ed2466899a59a4b83c39a6bc562a60f2d86d8c
esdurgadevi <esdurgadevi1@gmail.com>
2026-07-02 12:48:01 +0530

    update server to use the logger middleware

--- notification-app-be/server.js
+++ notification-app-be/server.js
@@ -1,6 +1,6 @@
 import express from "express"
 import dotenv from "dotenv";
-import { logger } from "../logging-middleware/src/index.js";
+import { loggerMiddleware } from "./middleware/loggerMiddleware.js";
 import notificationRoutes from "./routes/notificationRoutes.js";
 
 dotenv.config()
@@ -8,23 +8,7 @@ dotenv.config()
 const app = express();
 app.use(express.json());
 const port = process.env.PORT;
-app.post("/log", async (req, res) => {
-
-    try {
-
-        const response = await logger(req.body);
-
-        return res.status(200).json(response);
-
-    } catch (error) {
-
-        return res.status(500).json({
-            error: error.message
-        });
-
-    }
-
-});
+app.use(loggerMiddleware);
 app.use("/api", notificationRoutes);
 
 app.listen(port,()=>{
