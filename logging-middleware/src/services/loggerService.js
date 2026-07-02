e9da1363d91f35c89fec4165b7ba9fe58fe26299
esdurgadevi <esdurgadevi1@gmail.com>
2026-07-02 12:08:53 +0530

    fix logging middleware

--- logging-middleware/src/services/loggerService.js
+++ logging-middleware/src/services/loggerService.js
@@ -5,17 +5,19 @@ import { CONSTANTS } from "../log_constants.js";
 dotenv.config();
 
 export const loggerService = async (payload) => {
-
+    console.log(payload);
+    console.log(process.env.TOKEN);
     const response = await axios.post(
         CONSTANTS.LOG_API,
         payload,
         {
             headers: {
-                Authorization: `Bearer ${process.env.TOKEN}`,
+                Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiYXVkIjoiaHR0cDovLzIwLjI0NC41Ni4xNDQvZXZhbHVhdGlvbi1zZXJ2aWNlIiwiZW1haWwiOiIyMzEyMDQ3QG5lYy5lZHUuaW4iLCJleHAiOjE3ODI5NzQ3NDgsImlhdCI6MTc4Mjk3Mzg0OCwiaXNzIjoiQWZmb3JkIE1lZGljYWwgVGVjaG5vbG9naWVzIFByaXZhdGUgTGltaXRlZCIsImp0aSI6IjIwOWUwNzVhLTRjZWItNDMxNC1iOTRiLTMzYzdiYmNjYjMxNiIsImxvY2FsZSI6ImVuLUlOIiwibmFtZSI6ImR1cmdhIGRldmkgcyIsInN1YiI6Ijk5NGEzYzg4LWJhZjAtNDk1Yy05YWJhLWNhM2UwZDc5NmZiNSJ9LCJlbWFpbCI6IjIzMTIwNDdAbmVjLmVkdS5pbiIsIm5hbWUiOiJkdXJnYSBkZXZpIHMiLCJyb2xsTm8iOiIyMzEyMDQ3IiwiYWNjZXNzQ29kZSI6IkVSelV5eCIsImNsaWVudElEIjoiOTk0YTNjODgtYmFmMC00OTVjLTlhYmEtY2EzZTBkNzk2ZmI1IiwiY2xpZW50U2VjcmV0IjoiQlRQVU10dW5hdmV1QVFTcSJ9.YJfNgoPLYUglqxKvqHwMW6lTgxHFmsnKDRfJBrElHmA`,
                 "Content-Type": "application/json"
             }
         }
     );
+    console.log(response);
 
     return response.data;
 };
--- logging-middleware/src/utils/isValid.js
+++ logging-middleware/src/utils/isValid.js
@@ -1,7 +1,7 @@
 import { CONSTANTS } from "../log_constants.js";
 
-export const isValidParameters = (stack,level,package)=>{
+export const isValidParameters = (stack,level,packageName)=>{
     if(!CONSTANTS.stack.includes(stack) || !CONSTANTS.level.includes(level)) return false;
-    if(CONSTANTS.package.shared.includes(package)) return true;
-    return (stack === 'backend' && CONSTANTS.package.backend.includes(package)) || (stack === 'frontend' && CONSTANTS.package.frontend.includes(package));
+    if(CONSTANTS.package.shared.includes(packageName)) return true;
+    return (stack === 'backend' && CONSTANTS.package.backend.includes(packageName)) || (stack === 'frontend' && CONSTANTS.package.frontend.includes(packageName));
 }
